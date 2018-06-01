7bbbca0040cdd1474d87c007b56bd7f88a1c47b4
Edward G. Prins III <egprinsiii@gmail.com>
2018-06-01 09:09:22 -0500

    Added get all files, get file by id, and create file.

---
+++ back-end-java/src/main/java/com/cooksys/mydrive/service/FileService.java
@@ -0,0 +1,52 @@
+package com.cooksys.mydrive.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cooksys.mydrive.model.FileModel;
+import com.cooksys.mydrive.repository.FileRepository;
+
+@Service
+public class FileService {
+
+	private FileRepository fileRepository;
+	
+	public FileService(FileRepository fileRepository) {
+		this.fileRepository = fileRepository;
+	}
+
+	public ResponseEntity<?> getAll() {
+		List<FileModel> list = new ArrayList<>();
+		Iterable<FileModel> files = fileRepository.findAll();
+		files.forEach(list::add);
+		return ResponseEntity.ok(list);
+	}
+
+	public ResponseEntity<byte[]> getFile(Long id) {
+		Optional<FileModel> fileOptional = fileRepository.findById(id);
+		if (fileOptional.isPresent()) {
+			FileModel file = fileOptional.get();
+			return ResponseEntity.ok()
+					.body(file.getContent());
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	public ResponseEntity<?> createFile(MultipartFile file) {
+		try {
+			FileModel fileTemp = new FileModel(file.getOriginalFilename(), file.getContentType(), file.getBytes(), false);
+			fileRepository.save(fileTemp);
+			return ResponseEntity.ok(fileTemp.getId());
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
