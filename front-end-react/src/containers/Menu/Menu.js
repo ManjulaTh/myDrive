ce81bf53b2a84c102e7588a3f2822e214767b76e
Manjula Th <manjula_ss@yahoo.com>
2018-06-03 09:02:44 -0500

    still working

--- front-end-react/src/containers/Menu/Menu.js
+++ front-end-react/src/containers/Menu/Menu.js
@@ -3,51 +3,49 @@ import React from 'react';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import MenuItem from '@material-ui/core/MenuItem'
+
 
 class SimpleMenu extends React.Component {
     state = {
-        open: false,
+        anchorEl: null,
+    };
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
     };
 
     handleToggle = () => {
         this.setState({ open: !this.state.open });
     };
 
-    handleClose = event => {
-        if (this.target1.contains(event.target) || this.target2.contains(event.target)) {
-            return;
-        }
-
-        this.setState({ open: false });
+    handleClose = () => {
+        this.setState({ anchorEl: false });
     };
 
     render() {
-        const { classes } = this.props;
-        const { open } = this.state;
+        const { anchorEl } = this.state;
+
         return (
             <div>
-                <Button
+                {/* <Button
                     aria-owns={anchorEl ? 'simple-menu' : null}
                     aria-haspopup="true"
-                    onClick={this.handleClick}
+                    onClick={this.handleToggle}
                 >
-                    Open Menu
-        </Button>
-                <ClickAwayListener onClickAway={this.handleClose}>
-                    <Grow in={open} id="menu-list-grow" style={{ transformOrigin: '0 0 0' }}>
-                        <Menu
-                            id="simple-menu"
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl)}
-                            onClose={this.handleClose}
-                        >
-                            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                            <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                            <MenuItem onClick={this.handleClose}>Logout</MenuItem>
-                        </Menu>
-                    </Grow>
-                </ClickAwayListener>
+
+                </Button> */}
+                <Menu
+                    id="simple-menu"
+                    placement="bottom-start"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={this.handleClose}
+                >
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                </Menu>
             </div>
         );
     }
