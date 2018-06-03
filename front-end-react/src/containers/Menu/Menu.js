d44061a58bbf2413378df678508cdbdb1f430982
Manjula Th <manjula_ss@yahoo.com>
2018-06-02 19:59:44 -0500

    in progress 2

--- front-end-react/src/containers/Menu/Menu.js
+++ front-end-react/src/containers/Menu/Menu.js
@@ -1,40 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { Manager, Target, Popper } from 'react-popper';
 import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Collapse from '@material-ui/core/Collapse';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Portal from '@material-ui/core/Portal';
 import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { withStyles } from '@material-ui/core/styles';
 
-class SimpleMenu extends React.Component {
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    paper: {
+        marginRight: theme.spacing.unit * 2,
+    },
+    popperClose: {
+        pointerEvents: 'none',
+    },
+});
+
+class Menu extends React.Component {
     state = {
-        anchorEl: null
+        open: false,
     };
 
-    handleClick = event => {
-        this.setState({ anchorEl: event.currentTarget });
+    handleToggle = () => {
+        this.setState({ open: !this.state.open });
     };
 
-    handleClose = () => {
-        this.setState({ anchorEl: null });
+    handleClose = event => {
+
+
+        this.setState({ open: false });
     };
 
     render() {
-        const { anchorEl } = this.state;
+        const { classes } = this.props;
+        const { open } = this.state;
 
         return (
-            <div>
+            <div className={classes.root}>
 
-                <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={this.handleClose}
+
+
+                <Popper
+                    placement="bottom-start"
+                    eventsEnabled={open}
+                    className={classNames({ [classes.popperClose]: !open })}
                 >
-                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                    <MenuItem onClick={this.handleClose}>Logout</MenuItem>
-                </Menu>
+                    <ClickAwayListener onClickAway={this.handleClose}>
+                        <Grow in={open} id="menu-list-grow" style={{ transformOrigin: '0 0 0' }}>
+                            <Paper>
+                                <MenuList role="menu">
+                                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                                    <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                                </MenuList>
+                            </Paper>
+                        </Grow>
+                    </ClickAwayListener>
+                </Popper>
             </div>
-        );
+        )
     }
 }
 
-export default SimpleMenu;
+export default withStyles(styles)(Menu);
--- front-end-react/src/index.js
+++ front-end-react/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import './index.css'
 import App from './App'
 
 
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>,
+    <MuiThemeProvider><App /></MuiThemeProvider>,
     document.getElementById('root')
 )
